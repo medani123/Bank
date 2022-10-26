877e139f2c3441d2d21d8849681f6c913da1af19
Muhammad Medani <45421893+medani123@users.noreply.github.com>
2022-10-26 19:40:03 +0300

    Final commit

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        A better card deal with <br className='sm:block hidden' /> few easy
+        steps.
+      </h2>
+
+      <Button styles={`mt-10`} />
+    </div>
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt='billing' className='w-[100%] h-[100%]' />
+    </div>
+  </section>
+);
 
 export default CardDeal;
