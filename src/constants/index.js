5fad6b94f3af20cc1e0c040213a4a4f880f575fc
Muhammad Medani <45421893+medani123@users.noreply.github.com>
2022-10-26 19:20:11 +0300

    added Bussiness component

--- src/constants/index.js
+++ src/constants/index.js
@@ -89,17 +89,17 @@ export const stats = [
   {
     id: "stats-1",
     title: "User Active",
-    value: "3800+",
+    value: "5000+",
   },
   {
     id: "stats-2",
     title: "Trusted by Company",
-    value: "230+",
+    value: "100+",
   },
   {
     id: "stats-3",
     title: "Transaction",
-    value: "$230M+",
+    value: "$150M+",
   },
 ];
 
